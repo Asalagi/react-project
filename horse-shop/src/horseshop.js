bc319327272cbfae001f9d4496c5f087462a6b3a
Asalagi <mypaintedfrog@yahoo.com>
2023-08-01 12:12:33 -0500

    style in css

--- horse-shop/src/horseshop.js
+++ horse-shop/src/horseshop.js
@@ -1,10 +1,31 @@
 import './horse-shop.css';
 
 function HorseShop() {
+  
   return (
     <div>
-      <h1>Welcome</h1>
-      <p>This will be where horses can be bought</p>
+      <header class="header">
+        <h1>Welcome</h1>
+        <p>This will be where horses can be bought</p>
+          <div class="cart"><p>Cart (0)</p></div>
+      </header>
+      <div class="outer-box">
+      <div class="box">
+        <h2>Horse Name</h2>
+        <p>Horse info</p>
+        <button>Buy</button>
+      </div>
+      <div class="box">
+        <h2>Horse Name</h2>
+        <p>Horse info</p>
+        <button>Buy</button>
+      </div>
+      <div class="box">
+        <h2>Horse Name</h2>
+        <p>Horse info</p>
+        <button>Buy</button>
+      </div>
+      </div>
     </div>
   );
 }
