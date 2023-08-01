95604c90e1bdaea9fb8b09ed360a1c3e22461be1
Asalagi <mypaintedfrog@yahoo.com>
2023-08-01 12:54:16 -0500

    add image

--- horse-shop/src/horseshop.js
+++ horse-shop/src/horseshop.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import horseImage from './images/horse.jpg';
 import './horse-shop.css';
 
 function HorseShop() {
@@ -11,16 +13,19 @@ function HorseShop() {
       </header>
       <div class="outer-box">
       <div class="box">
+        <img class="horse" src={horseImage} alt="horse"></img>
         <h2>Horse Name</h2>
         <p>Horse info</p>
         <button>Buy</button>
       </div>
       <div class="box">
+      <img class="horse" src={horseImage} alt="horse"></img>
         <h2>Horse Name</h2>
         <p>Horse info</p>
         <button>Buy</button>
       </div>
       <div class="box">
+      <img class="horse" src={horseImage} alt="horse"></img>
         <h2>Horse Name</h2>
         <p>Horse info</p>
         <button>Buy</button>
