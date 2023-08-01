4d409cb35836b9493d1345a38df36ad483098ad1
Asalagi <mypaintedfrog@yahoo.com>
2023-08-01 15:51:13 -0500

    add button

--- horse-shop/src/horseshop.js
+++ horse-shop/src/horseshop.js
@@ -3,32 +3,32 @@ import horseImage from './images/horse.jpg';
 import './horse-shop.css';
 
 function HorseShop() {
-  
+
   return (
     <div>
       <header class="header">
         <h1>Welcome</h1>
         <p>This will be where horses can be bought</p>
-          <div class="cart"><p>Cart (0)</p></div>
+          <div class="cart"><p>Cart (0) $0</p></div>
       </header>
       <div class="outer-box">
       <div class="box">
         <img class="horse" src={horseImage} alt="horse"></img>
         <h2>Horse Name</h2>
         <p>Horse info</p>
-        <button>Buy</button>
+        <button class="buy-btn">Add to Cart</button>
       </div>
       <div class="box">
       <img class="horse" src={horseImage} alt="horse"></img>
         <h2>Horse Name</h2>
         <p>Horse info</p>
-        <button>Buy</button>
+        <button class="buy-btn">Add to Cart</button>
       </div>
       <div class="box">
       <img class="horse" src={horseImage} alt="horse"></img>
         <h2>Horse Name</h2>
         <p>Horse info</p>
-        <button>Buy</button>
+        <button class="buy-btn">Add to Cart</button>
       </div>
       </div>
     </div>
